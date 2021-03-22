14220c1ce6374b775dcf816ac16e6dee83406521
John Gleeson <59605829+johngleeson93@users.noreply.github.com>
2021-03-22 20:12:25 +0000

    Baseline repo
    Baseline repo

---
+++ app/controllers/poi.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const Poi = {
+  home: {
+    handler: function (request, h) {
+      return h.view("home", { title: "Welcome" });
+    },
+  },
+  report: {
+    handler: function (request, h) {
+      return h.view("report", {
+        title: "Islands  ",
+        pois: this.pois,
+      });
+    },
+  },
+  create: {
+    handler: function (request, h) {
+      const data = request.payload;
+      data.creator = this.currentUser;
+      this.pois.push(data);
+      return h.redirect("/report");
+    },
+  },
+};
+
+module.exports = Poi;
